e154b45cb7d6e62dd24636ac9e412384d18484d1
AndreDurao17 <58223458+AndreDurao17@users.noreply.github.com>
2021-07-10 11:38:36 +0100

    Add files via upload

---
+++ pap/config.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+    session_start();
+
+
+$host = "localhost"; 
+$user = "root"; 
+$password = ""; 
+$dbname = "pap"; 
+
+$con = mysqli_connect($host, $user, $password, $dbname);
+
+?>
---
+++ pap/js/trocarimg.js
@@ -0,0 +1,12 @@
+function img01() {
+    document.getElementById("trocarimg").src = "img/Aveiro/Aveiro_1.jpg";
+}
+function img02() {
+    document.getElementById("trocarimg").src = "img/Aveiro/Aveiro_2.jpg";
+}
+function img03() {
+    document.getElementById("trocarimg").src = "img/Aveiro/Aveiro_3.jpg";
+}
+function img04() {
+    document.getElementById("trocarimg").src = "img/Aveiro/Aveiro_4.jpg";
+}
